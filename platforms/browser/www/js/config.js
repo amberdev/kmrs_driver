f6cd940e4f01f499b5627cf2c089d5bbe184e5c2
Dipanshu <dipanshu.gupta@subcodevs.com>
2018-06-25 18:59:07 +0530

    kmrs driver app first commit

---
+++ platforms/browser/www/js/config.js
@@ -0,0 +1,8 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={	
+	'ApiUrl':"https://foodsquire.com/driver/api",	
+	'DialogDefaultTitle':"DriverApp",	
+	'PushProjectID':"491618252405",
+	'APIHasKey':"1234567890"
+};
